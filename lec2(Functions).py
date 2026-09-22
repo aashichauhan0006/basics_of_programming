5302558c1c1efb174f3fed70540f1d8fda06d4e8
Aashi Chauhan <aashichauhan0006@gmail.com>
2026-09-22 22:40:36 +0530

    Add files via upload

---
+++ lec2(Functions).py
@@ -0,0 +1,80 @@
+import math
+# FUNCTIONS 
+    # A function is a block of code designed to perform a specific task. As you'll see, functions will let you do roughly the same calculation multiple times without duplicating any code.
+
+# The argument is the name of the variable that will be used as input to the function. It is always enclosed in parentheses that apppear immediately after the name of the function. (Note that a function can also have no arguments, or it can have multiple arguments.
+
+# Syntax of function
+#     def function_name(argument):
+#         body of function
+#         return  #if needed
+
+# EXAMPLE 1
+
+# def add_five(input_var):
+#     output_var = input_var + 5
+#     return output_var
+
+# print(add_five(5675))
+
+
+# EXAMPLE 2
+
+# def sum(var1, var2):
+#     total = var1 + var2
+#     return total
+
+# total = sum(55,33)
+# print(total)
+
+
+# EXAMPLE 3
+
+# def greet(name):
+#     greeting = "Hello "+name+". How are you?"
+#     return greeting
+
+# print(greet("Aashi"))
+
+
+# EXAMPLE 4 :- Function with no arguments
+# def aashi():
+#     info = "Name - Aashi, Age - 30, Education - MCA"
+#     return info 
+
+# print(aashi()) 
+
+
+# EXAMPLE 5 :- Function with multiple arguments
+
+# def percentage(maths,physics,chemistry,biology):
+#     sum = maths+physics+chemistry+biology
+#     result = (sum/400)*100
+#     return result
+
+
+# aashi = percentage(99,87,96,89)
+# print(aashi)
+
+#EXAMPLE 6
+
+# def total_pay(num_hours):
+#     pay_beforetax = num_hours*15
+#     pay_aftertax = pay_beforetax*0.88
+#     return pay_aftertax
+#     # return (num_hours*15)*0.88
+
+
+# manisha = total_pay(60)
+# print(manisha)
+
+
+# SCOPE OF A VARIABLE :- Variables defined inside the function body cannot be accessed outside of the function.
+
+
+
+
+
+
+    
+    
